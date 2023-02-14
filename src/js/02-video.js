63513d767bd563576ee31315ee4ee496cc490df7
Mykola V <98426734+Nicolai2022@users.noreply.github.com>
2023-02-14 16:41:52 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+iframe.setAttribute("data-vimeo-id", "19231868");
+const player = new Player("vimeo-player", {
+    id: 19231868,
+});
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
